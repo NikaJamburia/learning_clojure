54575d494beb6fad79481ddadaeb5ff1eb894be1
nika-jamburia <nika.jamburia@twino.ge>
2022-09-13 15:10:47 +0400

    tic-tac-toe-core * Added functionality to figure out winner * Refactored to use just matrix internally

--- src/learning_clojure/ttt/tic_tac_toe_core.clj
+++ src/learning_clojure/ttt/tic_tac_toe_core.clj
@@ -1,37 +1,61 @@
 (ns learning-clojure.ttt.tic-tac-toe-core)
 
-(defn yx [x y]
+(defn first-by [prec col]
+  (first (filter prec col)))
+
+(defn yx [y x]
   {:pre [(and (>= x 0) (<= x 2))
          (and (>= y 0) (<= y 2))]}
   { :x x :y y})
 
-(defn cell [value coordinates]
-  { :value value :coordinates coordinates })
+(defn find-in-grid [grid coordinates]
+  (get (get grid (:y coordinates)) (:x coordinates)))
 
-(defn is-at? [cell coordinates]
-  (= (:coordinates cell) coordinates))
+(defn fill-cell [grid value coordinates]
+  {:pre [(= :e (find-in-grid grid coordinates))]}
+  (vec (map (fn [outer-index]
+              (let [inner-indexes [0 1 2]]
+                (vec (map (fn [inner-index]
+                            (if (= (yx outer-index inner-index) coordinates)
+                              value
+                              (get (get grid outer-index) inner-index)))
+                          inner-indexes))))
+            [0 1 2])))
 
-(defn find-in-matrix [matrix i1 i2]
-  (get (get matrix i1) i2))
+(defn all-values [grid]
+  (for [outer [0 1 2]
+        inner [0 1 2]]
+    (find-in-grid grid (yx outer inner))))
 
-(defn to-table [matrix]
-  [
-   (cell (find-in-matrix matrix 0 0) (yx 0 0)) (cell (find-in-matrix matrix 0 1) (yx 0 1)) (cell (find-in-matrix matrix 0 2) (yx 0 2))
-   (cell (find-in-matrix matrix 1 0) (yx 1 0)) (cell (find-in-matrix matrix 1 1) (yx 1 1)) (cell (find-in-matrix matrix 1 2) (yx 1 2))
-   (cell (find-in-matrix matrix 2 0) (yx 2 0)) (cell (find-in-matrix matrix 2 1) (yx 2 1)) (cell (find-in-matrix matrix 2 2) (yx 2 2))
-   ])
+(defn is-full? [grid]
+  (every? #(not= % :e) (all-values grid)))
 
+(defn get-rows[grid]
+  [(get grid 0) (get grid 1) (get grid 2)])
 
+(defn get-cols [grid]
+  (vec (for [col-index [0 1 2]]
+         [(find-in-grid grid (yx 0 col-index)) (find-in-grid grid (yx 1 col-index)) (find-in-grid grid (yx 2 col-index))])))
 
-(defn get-cell [table coordinates]
-  (first (filter #(is-at? % coordinates) table)))
+(defn get-diagonals [grid]
+  [[(find-in-grid grid (yx 0 0)) (find-in-grid grid (yx 1 1)) (find-in-grid grid (yx 2 2))]
+   [(find-in-grid grid (yx 0 2)) (find-in-grid grid (yx 1 1)) (find-in-grid grid (yx 2 0))]])
 
-(defn get-cell-value [table coordinates]
-  (:value (first (filter #(is-at? % coordinates) table))))
+(defn winner [cells]
+  (if (and (not= (get cells 0) :e) (and (= (get cells 0) (get cells 1)) (= (get cells 0) (get cells 2))))
+    (get cells 0)
+    nil))
 
-(defn fill-cell [table value coordinates]
-  {:pre [(= :e (:value (get-cell table coordinates)))]}
-  (map
-    (fn [old-cell] (if (is-at? old-cell coordinates)
-       (cell value coordinates)
-       old-cell)) table))
+(defn get-winner [grid]
+  (let [rows (get-rows grid)
+        cols (get-cols grid)
+        diagonals (get-diagonals grid)
+        rows-winner (first-by #(not= nil %) (map winner rows))
+        cols-winner (first-by #(not= nil %) (map winner cols))
+        diagonal-winner (first-by #(not= nil %) (map winner diagonals))]
+    (first-by #(not= nil %) [rows-winner cols-winner diagonal-winner])))
+
+(defn new-grid []
+  [[:e :e :e]
+   [:e :e :e]
+   [:e :e :e]])
--- test/learning_clojure/ttt/tic_tac_toe_core_test.clj
+++ test/learning_clojure/ttt/tic_tac_toe_core_test.clj
@@ -7,43 +7,112 @@
     (is (thrown? AssertionError (yx -1 2)))
     (is (thrown? AssertionError (yx 0 3)))
     (is
-      (= (yx 1 2) {:x 1 :y 2}))
+      (= (yx 1 2) {:x 2 :y 1}))
     )
   )
 
-(deftest matrix
-  (testing "Matrix is converted to table"
-    (let [result (to-table [[:x :e :x]
-                            [:o :x :e]
-                            [:x :e :o]])]
+(deftest grid-components
+  (testing "rows can be obtained from grid"
+    (let [rows (get-rows [[:x :o :e]
+                          [:x :x :x]
+                          [:e :e :o]])]
       (are [x y] (= x y)
-                 (get-cell-value result (yx 0 0)) :x
-                 (get-cell-value result (yx 0 1)) :e
-                 (get-cell-value result (yx 0 2)) :x
-                 (get-cell-value result (yx 1 0)) :o
-                 (get-cell-value result (yx 1 1)) :x
-                 (get-cell-value result (yx 1 2)) :e
-                 (get-cell-value result (yx 2 0)) :x
-                 (get-cell-value result (yx 2 1)) :e
-                 (get-cell-value result (yx 2 2)) :o
-                 ))))
+                 (count rows) 3
+                 (get rows 0) [:x :o :e]
+                 (get rows 1) [:x :x :x]
+                 (get rows 2) [:e :e :o]
+                 )))
 
-(deftest table
-  (let [table (to-table [[:x :o :x]
-                         [:x :x :x]
-                         [:x :e :o]])]
-    (testing "elements of table can be found by indexes"
+  (testing "columns can be obtained from grid"
+    (let [cols (get-cols [[:x :o :e]
+                          [:x :x :x]
+                          [:e :e :o]])]
       (are [x y] (= x y)
-                 (:value (get-cell table (yx 0 1))) :o
-                 (:value (get-cell table (yx 1 1))) :x
-                 (:value (get-cell table (yx 2 2))) :o
-                 (:value (get-cell table (yx 2 1))) :e))
+                 (count cols) 3
+                 (get cols 0) [:x :x :e]
+                 (get cols 1) [:o :x :e]
+                 (get cols 2) [:e :x :o]
+                 )))
+
+  (testing "diagonals can be obtained from grid"
+    (let [diagonals (get-diagonals [[:x :o :e]
+                                    [:x :x :x]
+                                    [:e :e :o]])]
+      (are [x y] (= x y)
+                 (count diagonals) 2
+                 (first diagonals) [:x :x :o]
+                 (second diagonals) [:e :x :e]
+                 )))
+
+  (testing "winner can be resolved given collection of cells"
+    (are [x y] (= x y)
+               (winner [:x :x :x]) :x
+               (winner [:o :o :o]) :o
+               (winner [:o :o :e]) nil
+               (winner [:e :e :e]) nil
+               ))
+  )
+
+(deftest grid
+  (let [grid [[:x :o :e]
+              [:x :x :x]
+              [:e :e :o]]]
+    (testing "elements of grid can be found by indexes"
+      (are [x y] (= x y)
+                 (find-in-grid grid (yx 0 1)) :o
+                 (find-in-grid grid (yx 1 1)) :x
+                 (find-in-grid grid (yx 2 2)) :o
+                 (find-in-grid grid (yx 2 1)) :e))
+
+    (testing "grid is not full when any of the elements is :e"
+      (is
+        (false? (is-full? grid))))
+
+    (testing "grid is full when none of the elements are :e"
+      (is
+        (true? (is-full? [[:x :o :x]
+                          [:x :x :x]
+                          [:x :x :o]]))))
+
+    (testing "if nobody has out a row, column or diagonal, winner is nil"
+      (is
+        (= (get-winner [[:x :o :x]
+                        [:x :e :x]
+                        [:e :x :o]]) nil)
+        ))
+
+    (testing "empty grid has no winners"
+      (is
+        (= (get-winner [[:e :e :e]
+                        [:e :e :e]
+                        [:e :e :e]]) nil)
+        ))
+
+    (testing "whatever player fills out a row, column or diagonal is a winner"
+      (are [x y] (= x y)
+                 (get-winner [[:x :o :x]
+                              [:x :x :x]
+                              [:e :x :o]]) :x
+
+                 (get-winner [[:x :o :x]
+                              [:x :o :x]
+                              [:e :o :o]]) :o
+
+                 (get-winner [[:e :x :e]
+                              [:e :x :e]
+                              [:e :x :e]]) :x
+
+                 (get-winner [[:o :o :x]
+                              [:x :o :x]
+                              [:x :x :o]]) :o
+                 ))
 
     (testing "elements cant be inserted in already filled cell"
-      (is (thrown? AssertionError (fill-cell table :x (yx 1 1)))))
+      (is (thrown? AssertionError (fill-cell grid :x (yx 1 1)))))
 
     (testing "elements can be inserted in empty cell"
       (are [x y] (= x y)
-                 (get-cell-value (fill-cell table :o (yx 2 1)) (yx 2 1)) :o
-                 )))
-  )
+                 (find-in-grid (fill-cell grid :o (yx 2 1)) (yx 2 1)) :o
+                 (find-in-grid (fill-cell grid :x (yx 0 2)) (yx  0 2)) :x
+                 ))))
+
